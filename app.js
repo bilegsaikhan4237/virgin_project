fecf8487cd74a79322e2bd78a5aea390f57e410b
bilege <sa.bilegsaikhan@yahoo.com>
2021-03-15 20:56:40 +0800

    Shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -6,9 +6,20 @@ var scores = [0, 0];
 var roundScore = 0;
 
 // Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй. 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-var dice = Math.floor(Math.random() * 6) + 1;
+var diceNumber = Math.floor(Math.random() * 6) + 1;
 
 /* <div class="player-score" id="score-0">43</div> */
-window.document.querySelector('#score-0').textContent = dice
-window.document.querySelector('#score-1').textContent = dice
-console.log('Шоо :' + dice);
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+document.querySelector(".dice").style.display = "none";
+
+var diceDom = document.querySelector(".dice")
+diceDom.style.display = "none";
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    document.querySelector(".dice").style.display = "block";
+    document.querySelector(".dice").src = "dice-" + diceNumber + ".png"
+});
